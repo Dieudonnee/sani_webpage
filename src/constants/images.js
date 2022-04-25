77ef82c9c6ca59f14f029cc7703606911047c731
Dieudonnee <75275692+Dieudonnee@users.noreply.github.com>
2022-04-25 10:16:28 +0200

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -34,13 +34,11 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
-import dih from '../assets/dih.png';
 import dihLogo from '../assets/dih-logo.png';
 
 export default {
   dihLogo,
   email,
-  dih,
   mobile,
   api,
   cpp,
